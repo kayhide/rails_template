ecf5fad97a67411b5db37a3d7d56d91309f36fd6
kayhide <kayhide@gmail.com>
2014-04-29 21:20:37 +0900

    add rspec settings.

--- main.rb
+++ main.rb
@@ -170,6 +170,38 @@ EOS
 end
 
 
+# rspec
+# ============================================================
+
+generate 'rspec:install'
+
+append_to_file '.rspec', <<EOS
+--format=Fuubar
+EOS
+
+inside 'spec' do
+  prepend_to_file 'spec_helper.rb', <<EOS
+require 'simplecov'
+SimpleCov.start 'rails'
+
+EOS
+
+  comment_lines 'spec_helper.rb', 'config.fixture_path'
+  comment_lines 'spec_helper.rb', 'rspec/autorun'
+
+  inject_into_file 'spec_helper.rb', <<EOS, before: /^end$/
+
+  config.before do
+    FactoryGirl.reload
+  end
+EOS
+
+
+  remove_comments 'spec_helper.rb'
+end
+
+remove_dir 'test'
+
 # remove .keep
 # ============================================================
 

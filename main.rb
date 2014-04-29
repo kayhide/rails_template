3f3eb49a49ad450fd641611f28f47936094c9640
kayhide <kayhide@gmail.com>
2014-04-29 20:40:28 +0900

    add db_create_user task for postgresql.

--- main.rb
+++ main.rb
@@ -150,3 +150,24 @@ Dir['config/environments/*.rb'].each do |f|
   remove_comments f
 end
 
+
+# postgresql
+# ============================================================
+
+if gem_for_database == 'pg'
+  rakefile 'db_create_user.rake', <<'EOS'
+namespace :db do
+  desc 'Creates postgres user for all environments.'
+  task :create_user => :environment do
+    Rails.configuration.database_configuration.map do |env, conf|
+      conf['username']
+    end.uniq.compact.each do |username|
+      `createuser -e -s -d #{username}`
+    end
+  end
+end
+EOS
+
+  remove_file 'lib/tasks/.keep'
+end
+

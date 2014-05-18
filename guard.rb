9e23fc0139d3071f144041a7ccb6dbfdd77e54bd
kayhide <kayhide@gmail.com>
2014-05-18 16:45:22 +0900

    update guard settings.

--- guard.rb
+++ guard.rb
@@ -1,6 +1,6 @@
 run 'bundle exec guard init'
 gsub_file 'Guardfile', /guard :rspec do/, <<EOS.strip
-guard :rspec, cmd: 'spring rspec' do
+guard :rspec, cmd: 'bundle exec spring rspec' do
 EOS
 
 inject_into_file 'Guardfile', <<EOS, after: /^guard :rspec.*\n(\n|  .*\n)*/

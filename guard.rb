79f1c011e71dc66e82113d8b06beb98417a9a4c6
kayhide <kayhide@gmail.com>
2015-11-30 20:59:48 +0545

    update guard settings.

--- guard.rb
+++ guard.rb
@@ -1,10 +1,10 @@
 run 'bundle exec guard init'
-gsub_file 'Guardfile', /guard :rspec do/, <<EOS.strip
-guard :rspec, cmd: 'bundle exec spring rspec' do
+gsub_file 'Guardfile', /guard :rspec .* do/, <<EOS.strip
+guard :rspec, cmd: 'bin/rspec' do
 EOS
 
 inject_into_file 'Guardfile', <<EOS, after: /^guard :rspec.*\n(\n|  .*\n)*/
 
   # FactoryGirl
-  watch(%r{^spec/factories/(.+)\\.rb$})                { |m| Dir["spec/controllers", "spec/requests"] }
+  watch(%r{^spec/factories/(.+)\\.rb$}) { |m| Dir["spec/controllers", "spec/requests"] }
 EOS
